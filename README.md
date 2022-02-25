2db8e0af727349d5f3ec9552772580f084277ea1
Alpha2Mao <2328119758@qq.com>
2022-02-25 22:33:04 +0800

    feature add thirdy party library which use cmake config

--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 ![GitHub](https://img.shields.io/github/license/Alpha2Mao/L-Bike)
 ![coverity](https://scan.coverity.com/projects/24516/badge.svg)
 
-
 bicycle GPS position upload
+
+## Introduction
+## Get Started
+### Build
+```shell
+cmake -Bbuild -Hiceoryx_meta -DCMAKE_PREFIX_PATH=$(PWD)/build/dependencies/ -DBUILD_TEST=ON -DINTROSPECTION=OFF -DBINDING_C=ON -DEXAMPLES=ON
+```
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include "include/glog/logging.h"
-#include "src/third/ThirdConfig.h"
+#include "tinyxml2.h"
+#include <glog/logging.h>
 
 using namespace google;
 
 int main(int argc, char **argv) {
-    ThirdConfig* thirdConfig = ThirdConfig::getInstance(argv);
+    InitGoogleLogging(argv[0]);
+    SetLogDestination(INFO, "./log/LBike_log");
+    FLAGS_alsologtostderr = true;
+    FLAGS_colorlogtostderr = true;
+    FLAGS_max_log_size = 1;
+    LOG(INFO) << "Hello New Bike, XML Library is init success";
     return 0;
 }
